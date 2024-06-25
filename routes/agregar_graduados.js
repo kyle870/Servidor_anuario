9406bb8101a3e904e18971dbd536446744bd1a32
osmar <osmarquintero87@gmail.com>
2024-06-25 10:50:54 -0600

    cambios en rutas de coleccion fotos y graduado

--- routes/agregar_graduados.js
+++ routes/agregar_graduados.js
@@ -13,7 +13,7 @@ router.get('/obtener', GraduadosController.obtenerGraduados);
 router.get('/buscar/imagen/:carnet', GraduadosController.verImagengraduado)
 
 //Editar/Actualizar graduado
-router.put('/actualizar/:id', subirImagen.single('foto_graduado'), GraduadosController.actualizarGraduado);
+router.put('/actualizar/:id', subirImagen.single('foto_graduado'), GraduadosController.updateGraduado);
 
 //Mostrar un graduado en especifico
 router.get('/obtener/:id', GraduadosController.obtenerUngraduado);
@@ -23,9 +23,6 @@ router.delete('/eliminar/:id', GraduadosController.eliminarGraduado);
 
 // Ruta para mostrar un graduado por carnet
 router.get('/buscar-carnet/:carnet', GraduadosController.mostrarPorCarnet);
-
-//Guardar datos desde un archivo de excel
-router.post('/guardar-desde-excel', GraduadosController.guardarDatosExcel);
 
 // Ruta para filtrar graduados
 router.get('/filtrar', GraduadosController.filtrarGraduados);
